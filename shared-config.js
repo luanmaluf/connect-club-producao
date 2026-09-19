6107de9fc351b26252499c272480d88b6a7d0a89
luanmaluf <luanmalufoficial@gmail.com>
2026-09-18 23:41:51 -0300

    Publica workspace compartilhado automático

--- shared-config.js
+++ shared-config.js
@@ -1,2 +1,3 @@
 window.CONNECT_CLUB_REMOTE_URL="https://superjsonblob.com/api/jsonBlob/332f9df6-1922-4b21-988d-dd4446ca15da";
 (function(){try{var u=new URL(window.location.href);if(!u.searchParams.get('db')){u.searchParams.set('db',window.CONNECT_CLUB_REMOTE_URL);history.replaceState(null,'',u.toString());}}catch(e){console.error(e);}})();
+// workspace compartilhado automático\n
